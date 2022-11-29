916802a3a37d396c329046a3ac39473d6a318b4f
Olga Panayot <panayot.olga@gmail.com>
2022-11-29 19:03:36 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+const galleryList = makeGalleryItems(galleryItems);
+refs.gallery.insertAdjacentHTML('beforeend', galleryList);
+
+function makeGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+			<img class="gallery__image" src="${preview}" alt="${description}" />
+			</a>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
